4f1a91bcc1cda814cc56fa4c62310a1640957543
edeyrnion <matthias.treubrodt@gmail.com>
2018-11-06 13:01:53 +0100

    Stuff

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -1,39 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Helpers
 {
-    public static Vector2 PointInCircle(float radius)
+    public static Vector2Int PointInCircle(int radius)
     {
         var t = 2 * Mathf.PI * Random.value;
         var r = radius * Mathf.Sqrt(Random.value);
 
-        return new Vector2(r * Mathf.Cos(t), r * Mathf.Sin(t));
+        var x = Mathf.FloorToInt(r * Mathf.Cos(t));
+        var y = Mathf.FloorToInt(r * Mathf.Sin(t));
+
+        return new Vector2Int(x, y);
     }
 
-    public static double NextRandom(double mean, double sigma)
+    public static Vector2Int PointInEllipse(int width, int height)
     {
-        double u, v, S;
+        var t = 2 * Mathf.PI * Random.value;
+        var r = Mathf.Sqrt(Random.value);
+
+        var x = Mathf.FloorToInt(width * r * Mathf.Cos(t));
+        var y = Mathf.FloorToInt(height * r * Mathf.Sin(t));
+
+        return new Vector2Int(x, y);
+    }
+
+    private static float NextRandom(float mean, float sigma)
+    {
+        float u, v, s;
 
         do
         {
-            u = 2.0 * Random.value - 1.0;
-            v = 2.0 * Random.value - 1.0;
-            S = u * u + v * v;
+            u = 2.0f * Random.value - 1.0f;
+            v = 2.0f * Random.value - 1.0f;
+            s = u * u + v * v;
         }
-        while (S >= 1.0 && S <= 3* sigma);
+        while (s >= 1.0f && s <= 3f * sigma);
 
-        double fac = System.Math.Sqrt(-2.0 * System.Math.Log(S) / S);
+        float fac = Mathf.Sqrt(-2.0f * Mathf.Log(s) / s);
         return ((u * fac) * sigma) + mean;
     }
 
-    public static double NormalizedRandom(double minValue, double maxValue)
+    public static int NormalizedRandom(int minValue, int maxValue)
     {
         var mean = (minValue + maxValue) / 2;
         var sigma = (maxValue - mean) / 3;
 
-        return NextRandom(mean, sigma);
+        return Mathf.FloorToInt(NextRandom(mean, sigma));
     }
 
     public static int RoundToGrid(float n, int gridSize)
@@ -41,15 +53,8 @@ public class Helpers
         return Mathf.FloorToInt(((n + gridSize - 1) / gridSize)) * gridSize;
     }
 
-    public static Vector2 RoundToGrid(Vector2 n, int gridSize)
+    public static Vector2Int RoundToGrid(Vector2 n, int gridSize)
     {
-        Vector2 v = new Vector2(RoundToGrid(n.x, gridSize), RoundToGrid(n.y, gridSize));
-        return v;
-    }
-
-    public static Vector3 RoundToGrid(Vector3 n, int gridSize)
-    {
-        Vector3 v = new Vector3(RoundToGrid(n.x, gridSize), RoundToGrid(n.y, gridSize), RoundToGrid(n.z, gridSize));
-        return v;
+        return new Vector2Int(RoundToGrid(n.x, gridSize), RoundToGrid(n.y, gridSize));
     }
 }
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,30 +4,51 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    private int numberOfRooms = 100;
-    private float radius = 100f;
+    private int ellipseWidth = 75;
+    private int ellipseHeight = 25;
 
-    private GameObject[] rooms;
+    private int numberOfRooms = 150;
+
+    private int roomMinWidth = 9;
+    private int roomMaxWidth = 26;
+    private int roomMinHeight = 4;
+    private int roomMaxHeight = 16;
+
+    private RectInt[] rooms;
 
     private void Start()
     {
-        rooms = new GameObject[numberOfRooms];
+        rooms = new RectInt[numberOfRooms];
 
         for (int i = 0; i < numberOfRooms; i++)
         {
-            Vector2 p = Helpers.PointInCircle(radius);
-            p = Helpers.RoundToGrid(p, 1);
+            Vector2Int pos = Helpers.PointInEllipse(ellipseHeight, ellipseWidth);
+            int width = Helpers.NormalizedRandom(roomMinWidth, roomMaxWidth);
+            int height = Helpers.NormalizedRandom(roomMinHeight, roomMaxHeight);
 
-            var x = (float)Helpers.NormalizedRandom(0, 11);
-            var z = (float)Helpers.NormalizedRandom(0, 11);
-
-            x = Helpers.RoundToGrid(x, 1);
-            z = Helpers.RoundToGrid(z, 1);
-
-            rooms[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            rooms[i].transform.parent = gameObject.transform;
-            rooms[i].transform.position = new Vector3(p.x, 0f, p.y);
-            rooms[i].transform.localScale = new Vector3(x, 3f, z);
+            rooms[i] = new RectInt(pos, new Vector2Int(width, height));
         }
+    }
+
+    public void Steer()
+    {
+        Vector3 avgDirection = Vector3.zero;
+        int numberOfCloseVehicles = 0;
+        for (int i = 0; i < vehicles.Count; i++)
+        {
+            Vector3 direction = vehicles[i].transform.position - transform.position;
+            float sqrDistance = direction.sqrMagnitude;
+            if (sqrDistance < (MaxSeparationDistance * MaxSeparationDistance) && sqrDistance > 0E-5f)
+            {
+                avgDirection += direction.normalized;
+                numberOfCloseVehicles++;
+            }
+        }
+        if (numberOfCloseVehicles == 0) { return; }
+        avgDirection /= numberOfCloseVehicles;
+        avgDirection = new Vector3(avgDirection.x, 0f, avgDirection.z);
+        Vector3 desired = avgDirection * (-1) * DesiredSpeed;
+        Vector3 steeringForce = Vector3.ClampMagnitude(desired - vehicle.Velocity, MaxForce);
+        vehicle.ApplyForce(steeringForce);
     }
 }
