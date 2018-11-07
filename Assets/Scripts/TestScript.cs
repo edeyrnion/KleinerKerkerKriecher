086aa20beb8f7eca37dc88268f684daf12280d27
edeyrnion <matthias.treubrodt@gmail.com>
2018-11-07 13:42:20 +0100

    Seperation

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -16,6 +16,8 @@ public class TestScript : MonoBehaviour
 
     private RectInt[] rooms;
 
+    private float time = 0;
+
     private void Start()
     {
         rooms = new RectInt[numberOfRooms];
@@ -26,29 +28,82 @@ public class TestScript : MonoBehaviour
             int width = Helpers.NormalizedRandom(roomMinWidth, roomMaxWidth);
             int height = Helpers.NormalizedRandom(roomMinHeight, roomMaxHeight);
 
-            rooms[i] = new RectInt(pos, new Vector2Int(width, height));
+            rooms[i] = new RectInt(pos, new Vector2Int(height, width));
         }
     }
 
-    public void Steer()
+    private void Update()
     {
-        Vector3 avgDirection = Vector3.zero;
-        int numberOfCloseVehicles = 0;
-        for (int i = 0; i < vehicles.Count; i++)
+        time += Time.deltaTime;
+        if (time > 0.05f)
         {
-            Vector3 direction = vehicles[i].transform.position - transform.position;
-            float sqrDistance = direction.sqrMagnitude;
-            if (sqrDistance < (MaxSeparationDistance * MaxSeparationDistance) && sqrDistance > 0E-5f)
-            {
-                avgDirection += direction.normalized;
-                numberOfCloseVehicles++;
-            }
+            time = 0f;
+            Seperation();
         }
-        if (numberOfCloseVehicles == 0) { return; }
-        avgDirection /= numberOfCloseVehicles;
-        avgDirection = new Vector3(avgDirection.x, 0f, avgDirection.z);
-        Vector3 desired = avgDirection * (-1) * DesiredSpeed;
-        Vector3 steeringForce = Vector3.ClampMagnitude(desired - vehicle.Velocity, MaxForce);
-        vehicle.ApplyForce(steeringForce);
+
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            DrawRect(rooms[i]);
+        }
+    }
+
+    private void Seperation()
+    {
+        int count = rooms.Length;
+        for (int i = 0; i < count; i++)
+        {
+            Vector2 avgDirection = Vector2.zero;
+            Vector2Int move;
+
+            for (int j = 0; j < count; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                float extendX = (rooms[i].width + rooms[j].width) * 0.5f;
+                float extendY = (rooms[i].height + rooms[j].height) * 0.5f;
+
+                float distanceX = Mathf.Abs(rooms[i].center.x - rooms[j].center.x);
+                float distanceY = Mathf.Abs(rooms[i].center.y - rooms[j].center.y);
+
+                if (distanceX < extendX && distanceY < extendY)
+                {
+                    Vector2 direction = rooms[j].position - rooms[i].position;
+                    avgDirection += direction.normalized;
+                }
+            }
+
+            if (avgDirection == Vector2.zero)
+            {
+                continue;
+            }
+
+            avgDirection *= -1f;
+
+            if (Mathf.Abs(avgDirection.x) >= Mathf.Abs(avgDirection.y))
+            {
+                move = new Vector2Int((int)(Mathf.Sign(avgDirection.x)), 0);
+            }
+            else
+            {
+                move = new Vector2Int(0, (int)(Mathf.Sign(avgDirection.y)));
+            }
+            rooms[i].position += move;
+        }
+    }
+
+    private void DrawRect(RectInt r)
+    {
+        Vector3 p1 = new Vector3(r.xMin, 0, r.yMin);
+        Vector3 p2 = new Vector3(r.xMin, 0, r.yMax);
+        Vector3 p3 = new Vector3(r.xMax, 0, r.yMax);
+        Vector3 p4 = new Vector3(r.xMax, 0, r.yMin);
+
+        Debug.DrawLine(p1, p2);
+        Debug.DrawLine(p2, p3);
+        Debug.DrawLine(p3, p4);
+        Debug.DrawLine(p4, p1);
     }
 }
